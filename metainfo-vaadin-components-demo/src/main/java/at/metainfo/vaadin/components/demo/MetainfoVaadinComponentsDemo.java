18a41ff55708c0224a418373cb1221c0a0be6eec
Thomas Kohler <thomas.kohler@generali.com>
2022-10-04 15:24:40 +0200

    Name refactoring

--- metainfo-vaadin-components-demo/src/main/java/at/metainfo/vaadin/components/demo/MetainfoVaadinComponentsDemo.java
+++ metainfo-vaadin-components-demo/src/main/java/at/metainfo/vaadin/components/demo/MetainfoVaadinComponentsDemo.java
@@ -18,7 +18,7 @@ import com.vaadin.flow.router.Route;
 import at.metainfo.color.Css3Color;
 import at.metainfo.enhanced.EnhancedDialog;
 import at.metainfo.enhanced.HasIconProvider.ViewIcon;
-import at.metainfo.enhanced.TabbedView;
+import at.metainfo.enhanced.EnhancedTabs;
 import at.metainfo.utilities.IGuiUtilities;
 import at.metainfo.vaadin.components.demo.views.TerminalView;
 import at.metainfo.vaadin.components.demo.views.TextAreaView;
@@ -30,51 +30,51 @@ public class MetainfoVaadinComponentsDemo extends Div implements IGuiUtilities {
 	public MetainfoVaadinComponentsDemo() {
 		setSizeFull();
 
-		TabbedView tv1 = createTabbedView(Red/*, Green, Blue*/);
-		TabbedView tv2 = createTabbedView(Yellow/*, Cyan, Magenta*/);
-		TabbedView tv3 = createTabbedView(Locale.ENGLISH, /*Black,*/ White);
-		TabbedView tv4 = createTabbedView(Locale.ENGLISH);
-		tv4.addView(new TerminalView());
+		EnhancedTabs ev1 = createEnhancedView(Red/*, Green, Blue*/);
+		EnhancedTabs ev2 = createEnhancedView(Yellow/*, Cyan, Magenta*/);
+		EnhancedTabs ev3 = createEnhancedView(Locale.ENGLISH, /*Black,*/ White);
+		EnhancedTabs ev4 = createEnhancedView(Locale.ENGLISH);
+		ev4.addView(new TerminalView());
 
-		tv1.getStyle().set("overflow", "hidden");
-		tv2.getStyle().set("overflow", "hidden");
-		tv3.getStyle().set("overflow", "hidden");
-		tv4.getStyle().set("overflow", "hidden");
+		ev1.getStyle().set("overflow", "hidden");
+		ev2.getStyle().set("overflow", "hidden");
+		ev3.getStyle().set("overflow", "hidden");
+		ev4.getStyle().set("overflow", "hidden");
 
-		SplitLayout vs1 = vs(tv1, tv2);
-		SplitLayout vs2 = vs(tv3, tv4);
+		SplitLayout vs1 = vs(ev1, ev2);
+		SplitLayout vs2 = vs(ev3, ev4);
 		SplitLayout hs = hs(vs1,vs2);
 
 		vs1.addSplitterDragendListener(event -> {
-			tv1.reset();
-			tv2.reset();
+			ev1.reset();
+			ev2.reset();
 		});
 		vs2.addSplitterDragendListener(event -> {
-			tv3.resize();
-			tv4.resize();
+			ev3.resize();
+			ev4.resize();
 		});
 		hs.addSplitterDragendListener(event -> {
-			tv1.resize();
-			tv2.resize();
-			tv3.resize();
-			tv4.resize();
+			ev1.resize();
+			ev2.resize();
+			ev3.resize();
+			ev4.resize();
 		});
 
 		add(hs);
 	}
 
-	private TabbedView createTabbedView(Css3Color... colors) {
-		return createTabbedView(Locale.forLanguageTag(Locale.getDefault().getLanguage()), colors);
+	private EnhancedTabs createEnhancedView(Css3Color... colors) {
+		return createEnhancedView(Locale.forLanguageTag(Locale.getDefault().getLanguage()), colors);
 	}
 
-	private TabbedView createTabbedView(Locale locale, Css3Color... colors) {
-		TabbedView tabbedView = new TabbedView(locale == Locale.ENGLISH ? getIconProvider() : null);
-		tabbedView.setSizeFull();
-		tabbedView.setDraggable(true);
-		tabbedView.setGlobalTarget(true);
+	private EnhancedTabs createEnhancedView(Locale locale, Css3Color... colors) {
+		EnhancedTabs enhancedView = new EnhancedTabs(locale == Locale.ENGLISH ? getIconProvider() : null);
+		enhancedView.setSizeFull();
+		enhancedView.setDraggable(true);
+		enhancedView.setGlobalTarget(true);
 
 		for(Css3Color color : colors) {
-			tabbedView.addView(new TextAreaView(color, locale));
+			enhancedView.addView(new TextAreaView(color, locale));
 		}
 
 		ComboBox<Css3Color> colorSelection = new ComboBox<>();
@@ -86,23 +86,23 @@ public class MetainfoVaadinComponentsDemo extends Div implements IGuiUtilities {
 				Notification.show("Select a background color first!", 2000, Position.MIDDLE);
 			} else {
 				TextAreaView view = new TextAreaView(colorSelection.getValue(), locale);
-				tabbedView.addView(view);
-				tabbedView.selectTab(view);
+				enhancedView.addView(view);
+				enhancedView.selectTab(view);
 				colorSelection.clear();
 			}
 		});
 		Button term = new Button("Add Terminal", click -> {
 			TerminalView terminal = new TerminalView();
-			tabbedView.addView(terminal);
-			tabbedView.selectTab(terminal);
+			enhancedView.addView(terminal);
+			enhancedView.selectTab(terminal);
 		});
 		Button dialog = new Button("Dialog", click -> {
 			new EnhancedDialog(new TextAreaView(Cyan, Locale.ENGLISH)).open();
 		});
-		tabbedView.addToToolbar(colorSelection, add, term, dialog);
-		tabbedView.setMinimizeable(true);
+		enhancedView.addToToolbar(colorSelection, add, term, dialog);
+		enhancedView.setMinimizeable(true);
 
-		return tabbedView;
+		return enhancedView;
 	}
 
 	private Function<ViewIcon, Component> getIconProvider() {
